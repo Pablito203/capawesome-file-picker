fded5d7e3dd7a0082c1397d2791a78147c2352ed
Pablo Martins <pablo.martins.01tec@gmail.com>
2023-12-18 15:00:12 -0300

    dvd

--- android/src/main/java/io/capawesome/capacitorjs/plugins/filepicker/GalleryActivity.java
+++ android/src/main/java/io/capawesome/capacitorjs/plugins/filepicker/GalleryActivity.java
@@ -1,19 +1,31 @@
 package io.capawesome.capacitorjs.plugins.filepicker;
 
+import static android.widget.AbsListView.OnScrollListener.SCROLL_STATE_IDLE;
+
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.widget.Toolbar;
 
 import androidx.appcompat.app.ActionBar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.Loader;
 
 
-public class GalleryActivity extends AppCompatActivity {
+public class GalleryActivity extends AppCompatActivity implements OnItemClickListener, LoaderManager.LoaderCallbacks<Cursor> {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -33,11 +45,39 @@ public class GalleryActivity extends AppCompatActivity {
         dialogBuilder.show();
 
         setupHeader();
+
+        GridView gridView = (GridView) findViewById(R.id.gridview);
+        gridView.setOnItemClickListener(this);
+        gridView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            private int lastFirstItem = 0;
+            private long timestamp = System.currentTimeMillis();
+
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+                if (scrollState == SCROLL_STATE_IDLE) {
+                    //shouldRequestThumb = true;
+                    //ia.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                float dt = System.currentTimeMillis() - timestamp;
+                if (firstVisibleItem != lastFirstItem) {
+                    double speed = 1 / dt * 1000;
+                    lastFirstItem = firstVisibleItem;
+                    timestamp = System.currentTimeMillis();
+
+                    // Limit if we go faster than a page a second
+                    //shouldRequestThumb = speed < visibleItemCount;
+                }
+            }
+        });
     }
 
     private void setupHeader() {
         LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
-        View header = findViewById(R.id.action_bar);
+        View header = inflater.inflate(R.layout.actionbar, null);
 
         // Show the custom action bar view and hide the normal Home icon and title.
         ActionBar actionBar = getSupportActionBar();
@@ -53,5 +93,26 @@ public class GalleryActivity extends AppCompatActivity {
                     ViewGroup.LayoutParams.MATCH_PARENT
             ));
         }
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @NonNull
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+
     }
 }
