9a7e1b148fbdc9244ad3142b6fdf4911fda69b3a
Pablo Martins <pablo.martins.01tec@gmail.com>
2023-12-29 16:19:21 -0300

    faff

--- android/src/main/java/io/capawesome/capacitorjs/plugins/filepicker/GalleryActivity.java
+++ android/src/main/java/io/capawesome/capacitorjs/plugins/filepicker/GalleryActivity.java
@@ -273,17 +273,6 @@ public class GalleryActivity extends AppCompatActivity implements OnItemClickLis
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
-        dialogBuilder.setTitle("Botão avançar");
-        dialogBuilder.setMessage("Botão avançar pressionado");
-        dialogBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.cancel();
-            }
-        });
-        dialogBuilder.create();
-        dialogBuilder.show();
-
         if (resultCode != RESULT_OK)
             return;
 
